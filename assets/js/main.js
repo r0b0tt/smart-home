746239b5c89fbb6a6890c94addb3952fba41d5b5
r0b0t <antoniomainakn@gmail.com>
2018-02-21 10:16:37 +0300

    Added a side-nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
     $('#fullpage').fullpage();
 });
+
+function openNav() {
+    /**
+     * Open the side nav bar
+     */
+    document.querySelector('#side-nav').style.display = "block";
+    document.querySelector('#side-nav').style.width = "300px";
+
+    document.querySelector('#open-nav').style.display = "none";
+}
+
+function closeNav() {
+    /**
+     * Close the side nav bar
+     */
+    document.querySelector('#side-nav').style.display = "none";
+    document.querySelector('#side-nav').style.width = "0";
+
+    document.querySelector('#open-nav').style.display = "block";
+}
